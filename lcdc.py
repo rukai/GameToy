ac2b26becc07b387986c279b1f8ef03f2a387829
Rukai <rubickent@gmail.com>
2016-04-12 22:49:55 +1000

    Implement rendering background and window

--- lcdc.py
+++ lcdc.py
@@ -9,13 +9,13 @@ class LCDC:
         # w - window
         # bg - background
         self.display_enable             = False
-        self.w_tile_map_display_select  = False
+        self.w_tile_map_select          = False
         self.w_display_enable           = False
-        self.bg_w_tile_select           = False
-        self.bg_tile_map_display_select = False
+        self.bg_w_tile_data_select      = False
+        self.bg_tile_map_select         = False
         self.sprite_size                = False
         self.sprite_display_enable      = False
-        self.bg_display                 = False
+        self.bg_display_enable          = False
 
         # FF41 bits 6-0
         self.enable_lyc_interrupt    = False
@@ -49,22 +49,78 @@ class LCDC:
         self.bgp_color2 = 0
         self.bgp_color3 = 0
 
-        ##FF48 bits
-        #color0 is unused because sprites render it as transparent
+        # FF48 bits
+        # color0 is unused because sprites render it as transparent
         self.obp0_color1 = 0
         self.obp0_color2 = 0
         self.obp0_color3 = 0
 
-        ##FF49 bits
+        # FF49 bits
         self.obp1_color1 = 0
         self.obp1_color2 = 0
         self.obp1_color3 = 0
 
-        self.screen = pygame.display.set_mode((160, 144))
+        self.screen = pygame.Surface((160, 144),)
+        self.display = pygame.display.set_mode((160 * 4, 144 * 4))
+        pygame.display.set_caption("GAMETOY")
+        self.emu_pallete = [
+            [224, 248, 208],
+            [136, 192, 112],
+            [ 48, 104,  80],
+            [  8,  24,  32],
+        ]
 
     def render(self):
-        self.screen.fill((255, 0, 0))
+        if self.display_enable:
+            self.screen.fill((255, 0, 255)) # self.screen.fill(self.emu_pallete[0])
+            if self.sprite_display_enable:
+                self.renderSprites()
+            if self.bg_display_enable:
+                self.renderBG_W(self.bg_tile_map_select, self.readSCX(), self.readSCY())
+            if self.w_display_enable:
+                self.renderBG_W(self.w_tile_map_select, self.readWX(), self.readWY())
+        else:
+            self.screen.fill((255, 0, 0)) # TODO: remove debug color
+        upscaled = pygame.transform.scale(self.screen, (self.screen.get_width() * 4, self.screen.get_height() * 4),)
+        self.display.blit(upscaled, (0, 0))
         pygame.display.flip()
+
+    def renderSprites(self):
+        tile_width = 16 if self.sprite_size else 8
+
+    def renderBG_W(self, map_select, x_offset, y_offset):
+        if map_select:
+            start_address = 0x9800
+        else:
+            start_address = 0x9C00
+
+        if self.bg_w_tile_data_select:
+            tile_start_address = 0x8000
+            read = self.mem.read
+        else:
+            tile_start_address = 0x8800
+            read = self.mem.readSigned
+        
+        for y in range(32):
+            for x in range(32):
+                address = start_address + 32 * y + x
+                tile_address = tile_start_address + read(address)
+                tile = self.renderTile(tile_address)
+                self.screen.blit(tile, (x * 8 + x_offset, y * 8 + y_offset))
+
+    def renderTile(self, address):
+        tile = pygame.Surface((8, 8))
+        for y in range(8):
+            address_y = address + y * 2
+            low_byte  = self.mem.read(address_y)
+            high_byte = self.mem.read(address_y + 1)
+            for x in range(8):
+                mask = 1 << x
+                low_bit = int(bool(low_byte & mask))
+                high_bit = int(bool(high_byte & mask))
+                color = (high_bit << 1) | low_bit
+                tile.set_at((y, x), self.emu_pallete[color])
+        return tile
 
     def update(self, cycles):
         self.mode_counter += cycles
@@ -110,24 +166,24 @@ class LCDC:
     # LCD Control
     def readLCDC(self):
         value =  int(self.display_enable)             << 7
-        value |= int(self.w_tile_map_display_select)  << 6
+        value |= int(self.w_tile_map_select)          << 6
         value |= int(self.w_display_enable)           << 5
-        value |= int(self.bg_w_tile_select)           << 4
-        value |= int(self.bg_tile_map_display_select) << 3
+        value |= int(self.bg_w_tile_data_select)      << 4
+        value |= int(self.bg_tile_map_select)         << 3
         value |= int(self.sprite_size)                << 2
         value |= int(self.sprite_display_enable)      << 1
-        value |= int(self.bg_display)                 << 0
+        value |= int(self.bg_display_enable)          << 0
         return value
 
     def writeLCDC(self, value):
         self.display_enable             = bool(value & 0b10000000)
-        self.w_tile_map_display_select  = bool(value & 0b01000000)
+        self.w_tile_map_select          = bool(value & 0b01000000)
         self.w_display_enable           = bool(value & 0b00100000)
-        self.bg_w_tile_select           = bool(value & 0b00010000)
-        self.bg_tile_map_display_select = bool(value & 0b00001000)
+        self.bg_w_tile_data_select      = bool(value & 0b00010000)
+        self.bg_tile_map_select         = bool(value & 0b00001000)
         self.sprite_size                = bool(value & 0b00000100)
         self.sprite_display_enable      = bool(value & 0b00000010)
-        self.bg_display                 = bool(value & 0b00000001)
+        self.bg_display_enable          = bool(value & 0b00000001)
 
     # LCD Status
     def readSTAT(self):
